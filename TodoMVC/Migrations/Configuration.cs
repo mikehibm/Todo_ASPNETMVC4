508f825eab4bd3e44be8aa32b69e7760c319e517
Makoto Ishida <makotoishida@gmail.com>
2012-11-22 16:37:05 -1000

    Enable CodeFirst Migration.

---
+++ TodoMVC/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace TodoMVC.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TodoMVC.Models.TodoEntities>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(TodoMVC.Models.TodoEntities context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
---
+++ TodoMVC/Models/TodoEntities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TodoMVC.Models {
+
+    public class TodoEntities : DbContext {
+
+        public TodoEntities()
+            : base("DefaultConnection") {
+        }
+    }
+}
